79df7091de34269cd7fb4c5152cebea2c315bb8c
Avnishpat <110018459+Avnishpat@users.noreply.github.com>
2023-05-27 01:00:40 +0530

    Create Hindi.sql

---
+++ Hindi.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE PACKAGE BODY xxdon_sample_japanese_pkg IS 
+
+PROCEDURE dummy IS 
+
+l_my_Hindi_string VARCHAR2(4000);
+
+BEGIN 
+
+  l_my_HIndi_string :=नमस्ते, आप कैसे हैं आप किस पर काम कर रहे हैं आप किस शहर से हो मुझे तुम्हारे बारे में कुछ बताओ:
+END;
+
+END;
+/
